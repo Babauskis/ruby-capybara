134975059fed4a204e70bdbf0f9937399f5be553
Sandis Babauskis <v-sababa@microsoft.com>
2018-12-28 11:43:36 +0200

    test

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -5,7 +5,6 @@ class HomePage < BasePage
     @button_logo = Element.new(:css, ".logo")
     @input_search = Element.new(:css, "#search_products_text_field")
     @button_search = Element.new(:css, "#autocomplete_form > div > input[type='submit']:nth-child(9)")
-    @price_for_first_item = Element.new(:xpath, '//a[contains(., "JBL Tune 500BT Bluetooth On-Ear Headphones Black")]/ancestor::div[@class="p-content"]/following-sibling::div[@class="info-holder"]/child::div[@class="p-info"]//child::div[@data-sell-price-w-vat="41.99"]'
   end
 
   def isVisible
